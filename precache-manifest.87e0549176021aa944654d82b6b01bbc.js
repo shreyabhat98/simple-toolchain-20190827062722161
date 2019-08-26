415f038a7b8a645f3928cdcaab66b71b88327ed4
rayshu <er.rshmi@gmail.com>
2019-08-26 12:39:30 +0530

    Updates

---
+++ precache-manifest.87e0549176021aa944654d82b6b01bbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799fa3a11704c268ddfa9f1949cf77b6",
+    "url": "/rayshu/Hiring-Tool/index.html"
+  },
+  {
+    "revision": "0d206fca056a91b7c142",
+    "url": "/rayshu/Hiring-Tool/static/css/2.9798096d.chunk.css"
+  },
+  {
+    "revision": "41be796a4fb967655c5f",
+    "url": "/rayshu/Hiring-Tool/static/css/main.eafdc888.chunk.css"
+  },
+  {
+    "revision": "0d206fca056a91b7c142",
+    "url": "/rayshu/Hiring-Tool/static/js/2.6214c914.chunk.js"
+  },
+  {
+    "revision": "41be796a4fb967655c5f",
+    "url": "/rayshu/Hiring-Tool/static/js/main.8806ec6d.chunk.js"
+  },
+  {
+    "revision": "4f757e6cc17268c0053e",
+    "url": "/rayshu/Hiring-Tool/static/js/runtime~main.4fed7629.js"
+  },
+  {
+    "revision": "3f7c9562a869481f4236f3a3b326f7f1",
+    "url": "/rayshu/Hiring-Tool/static/media/RBS-logo.3f7c9562.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/rayshu/Hiring-Tool/static/media/tryDownload.d41d8cd9.txt"
+  }
+]);
